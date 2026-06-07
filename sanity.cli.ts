021c04d5ea2ace587ae25e93477d06aa7f7c480d
Guybertho Clerger <guybertho@aguyb.com>
2026-06-07 03:22:56 +0000

    feat: add sanity.cli.ts for studio deployment

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '24ezss24',
+    dataset: 'production',
+  },
+  studioHost: 'jaxunico',
+})
